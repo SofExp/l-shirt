d7cfb194c254da31284e9171e79304ce2432a050
Dmitrii <vesseslaur@gmail.com>
2022-03-01 22:39:22 +0700

    01.03/1

--- scripts/color-choise.js
+++ scripts/color-choise.js
@@ -3,11 +3,11 @@
   const elem = document.querySelector('.choice__view__list');
   const iso = new Isotope(elem, {
       itemSelector: '.choice__view__item',
-      filter: '.green'
+      filter: '.black'
   });
 
   const controlls = document.querySelectorAll('.choise__color__link');
-  const activClass = 'choise__color__item-activ';
+  const activClass = 'choise__color__item-active';
 
   controlls.forEach(function(control) {
     control.addEventListener('click', function(e){
